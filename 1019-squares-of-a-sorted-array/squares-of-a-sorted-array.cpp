ddfa9fdb758dcc6e50c19509e3de7c9b0ad280e0
Shriya969 <shriyagoyal131@gmail.com>
2026-04-03 21:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (98.77%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,27 +1,39 @@
- class Solution {
+class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
-        int j=n-1;
-        int pos=n-1;
-        int right;int left;
+        int n = nums.size();
+
+        // Result array to store sorted squares
         vector<int> result(n);
-      while(i<=j)
-      {
-      left=nums[i]*nums[i];
-      right=nums[j]*nums[j];
-      if(left>right)
-      {
-        result[pos--]=left;
-        i++;
-      }
-      else
-      {
-        result[pos--]=right;
-        j--;
-      }
+
+        // Two pointers:
+        // i → start of array (smallest / most negative)
+        // j → end of array (largest positive)
+        int i = 0, j = n - 1;
+
+        // Position to fill in result array (from end to start)
+        int pos = n - 1;
+
+        // Process until both pointers cross
+        while (i <= j) {
+
+            // Square values at both ends
+            int leftSquare = nums[i] * nums[i];
+            int rightSquare = nums[j] * nums[j];
+
+            // Compare squares and place the larger one at current position
+            if (leftSquare > rightSquare) {
+                result[pos] = leftSquare; // place larger value
+                i++;                     // move left pointer forward
+            } else {
+                result[pos] = rightSquare;
+                j--;                     // move right pointer backward
+            }
+
+            pos--; // move to next position from end
+        }
+
+        // Return the sorted squares array
+        return result;
     }
-    return result;
-    }
-        };
+};
