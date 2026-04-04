0bc1c8843f0557be908325f01da0466a57fae3e0
Shriya969 <shriyagoyal131@gmail.com>
2026-04-04 23:48:51 +0530

    Time: 14 ms (12.92%) | Memory: 13.9 MB (88.65%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -9,8 +9,10 @@ public:
         for (int i = 0; i < n; i++) {
             if (arr[i] == 0) {
                 // Shift elements to the right
-                for (int j = n - 1; j > i; j--) {
-                    arr[j] = arr[j - 1];
+                int j = n - 1;
+                while (j > i) {
+                arr[j] = arr[j - 1];
+                j--; // decrement j to move left
                 }
                 // Duplicate zero
                 if (i + 1 < n) arr[i + 1] = 0;
