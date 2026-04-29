74bc161c16df862ab7b42e98bfcc898452917de3
Shriya Agarwal <shriyagoyal131@gmail.com>
2026-04-30 00:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (14.02%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+       int m=grid.size() ;
+       int n=grid[0].size();
+       for(int i=0;i<m;i++)
+       {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j || i+j==n-1)
+            {
+                if(grid[i][j]==0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(grid[i][j]!=0)
+                {
+                    return false;
+                }
+            }
+        }
+       }
+        return true;
+    }
+};
