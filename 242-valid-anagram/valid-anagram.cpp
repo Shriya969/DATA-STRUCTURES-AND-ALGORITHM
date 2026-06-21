144b601c4c1690aeeeb5133757c5d4a5040b3b97
Shriya Agarwal <shriyagoyal131@gmail.com>
2026-06-21 17:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // If lengths differ, strings cannot be anagrams
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        int freq[26] = {0};  // Frequency array for lowercase letters
-
-        // Count characters in the first string
-        for (int i = 0; i < s.length(); i++) {
-            char c = s[i];
-            freq[c - 'a']++;// 'a' maps to index 0, 'b' to 1, ..., 'z' to 25
-        }
-
-        // Subtract character counts using the second string
-        for (int i = 0; i < t.length(); i++) {
-            char c = t[i];
-            freq[c - 'a']--;
-        }
-
-        // Check if all frequencies are zero
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0) {
-                return false;  // Mismatch found
-            }
-        }
-
-        return true;  // Strings are anagrams
+      int n=s.size();
+      int m=t.size();
+      unordered_map<char,int> mp1;
+      unordered_map<char,int> mp2;
+      for(char ch:s) 
+      {
+        mp1[ch]++;
+      }
+      for(char ch:t) 
+      {
+        mp2[ch]++;
+      }
+      if(mp1==mp2)
+      {
+        return true;
+      }
+      return false;
     }
 };
